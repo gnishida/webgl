4a5598432c495e0fc392a8c102549e73c9747588
gnishida <gnishida@purdue.edu>
2015-03-24 18:05:27 -0400

    add ip_game

---
+++ ip_game/gui.js
@@ -0,0 +1,28 @@
+function ZoningGUI() {
+	var controls = {
+		gui: null,
+		"Zone": 0
+	};
+	
+	var init = function() {
+		controls.gui = new dat.GUI();
+
+		var zonings = controls.gui.addFolder('Zoning');
+
+		zonings.add(controls, "Zone", {Residential: 0, Commercial: 1, Industrial: 2, Park: 3}).onChange( controls.zoneChanged );
+
+		zonings.open();
+	}
+	
+	controls.zoneChanged = function() {
+		var data = {
+			detail: {
+				value: controls["Zone"]
+			}
+		}
+
+		window.dispatchEvent( new CustomEvent( 'zone_type_changed', data ) );
+	}
+	
+	init.call(this);
+}
